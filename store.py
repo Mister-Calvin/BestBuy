226b6b44c37a8d78af0e1ad84590ece81f1ae0f1
Calvin Kuven <calle@MacBook-Air-von-Calvin.local>
2025-09-14 18:41:19 +0200

    Create Store class
    - Implemented Store class with product management.
    - Added methods: add_product, remove_product, get_total_quantity, get_all_products, and order.
    - Supports handling multiple Product objects and processing orders.

---
+++ store.py
@@ -0,0 +1,34 @@
+from typing import List
+from products import Product
+
+
+
+class Store:
+    def __init__(self, products):
+        self.products = products
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def remove_product(self, product):
+        self.products.remove(product)
+
+    def get_total_quantity(self) -> int:
+        total = 0
+        for product in self.products:
+            total += product.get_quantity()
+        return total
+
+    def get_all_products(self) -> List[Product]:
+        products = []
+        for product in self.products:
+            if product.is_active():
+                products.append(product)
+        return products
+
+    def order(self, shopping_list) -> float:
+        total_price = 0.0
+        for product, quantity in shopping_list:
+            total_price += product.buy(quantity)
+        return total_price
+
