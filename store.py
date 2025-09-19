e1a09f6c9ff22c2ccea2bc01f06b087cc7e894ab
Calvin Kuven <calle@MacBook-Air-von-Calvin.local>
2025-09-19 18:39:35 +0200

    Add detailed docstrings to Store class methods

--- store.py
+++ store.py
@@ -1,34 +1,63 @@
-from typing import List
 from products import Product
-
+from typing import List
 
 
 class Store:
-    def __init__(self, products):
-        self.products = products
+    """
+    Initializes the Store with a list of Product objects.
+    Args:
+        product (list): A list of Product instances to be managed by the store.
+    """
+    def __init__(self, product):
+        self.product = product #list of Products
 
-    def add_product(self, product):
-        self.products.append(product)
 
-    def remove_product(self, product):
-        self.products.remove(product)
+    def add_product(self, products):
+        """ Adds a list of Product instances to the store.
+        Args: products (list): A list of Product instances to be added to the store."""
+        self.product.append(products)
+
+
+    def remove_product(self, products):
+        """ Removes a list of Product instances from the store.
+        Args: products (list): A list of Product instances to be removed from the store."""
+        self.product.remove(products)
+
 
     def get_total_quantity(self) -> int:
+        """ Returns the total quantity of the store."""
         total = 0
-        for product in self.products:
+        for product in self.product:
             total += product.get_quantity()
         return total
 
+
     def get_all_products(self) -> List[Product]:
+        """ Returns a list of all products in the store."""
         products = []
-        for product in self.products:
+        for product in self.product:
             if product.is_active():
                 products.append(product)
         return products
 
+
     def order(self, shopping_list) -> float:
+        """
+        Processes an order for multiple products and quantities.
+
+        Args:
+            shopping_list (list of tuples): Each tuple contains a Product and the quantity to buy.
+
+        Returns:
+            float: The total price of the purchase.
+
+        Raises:
+            Exception: If a product cannot fulfill the requested quantity (handled by Product.buy).
+        """
         total_price = 0.0
         for product, quantity in shopping_list:
             total_price += product.buy(quantity)
         return total_price
+
+
 
